8933702082af1275de01606c7ede7d5f7bb38427
Steven Buss <steven.buss@gmail.com>
2013-11-18 23:29:55 -0800

    Add test service and tastypie to installed apps

--- tests/django_test_service/django_test_service/settings.py
+++ tests/django_test_service/django_test_service/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myservice',
+    'tastypie',
 )
 
 MIDDLEWARE_CLASSES = (
